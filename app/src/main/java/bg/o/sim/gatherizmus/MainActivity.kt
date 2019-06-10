b6ecb325e70613343ebe4d1d8a4aa191a2e24852
simo-andreev <simeon.zlatanov.andreev@gmail.com>
2019-06-10 05:56:10 +0300

    style: MISC - formatting.

--- app/src/main/java/bg/o/sim/gatherizmus/MainActivity.kt
+++ app/src/main/java/bg/o/sim/gatherizmus/MainActivity.kt
@@ -6,18 +6,13 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.util.Xml
-import android.view.Menu
-import android.view.MenuItem
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import java.lang.StringBuilder
-import android.webkit.WebView
-import android.webkit.WebViewClient
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -46,34 +41,12 @@ class MainActivity : AppCompatActivity() {
         webContent.canGoBack()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode != Activity.RESULT_OK) {
-            toast("FAILED")
-            TODO()
-            return
-        }
+        if (resultCode != Activity.RESULT_OK) TODO()
 
-
-        val bmp = data!!.extras.get("data") as Bitmap
-
+        val bmp = data?.extras?.get("data") as Bitmap
 
         val visionImage = FirebaseVisionImage.fromBitmap(bmp)
         val reader = FirebaseVision.getInstance().onDeviceTextRecognizer
@@ -81,7 +54,8 @@ class MainActivity : AppCompatActivity() {
         val result = reader.processImage(visionImage)
             .addOnSuccessListener { firebaseVisionText ->
                 toast(firebaseVisionText.text)
-//                https://gatherer.wizards.com/Pages/Search/Default.aspx?name=+\[Angrath,\]+\[Minotaur\]+\[Pirate\]
+                // example URL
+                // https://gatherer.wizards.com/Pages/Search/Default.aspx?name=+\[Angrath,\]+\[Minotaur\]+\[Pirate\]
                 val builder = StringBuilder()
                 firebaseVisionText.text.split(" ").forEach {
                     builder.append("+[").append(it).append("]")
@@ -90,11 +64,8 @@ class MainActivity : AppCompatActivity() {
             }
             .addOnFailureListener {
                 // Task failed with an exception
-                // ...
                 toast(it.localizedMessage)
-                Log.e("ERR", "Processing fail", it)
+                Log.e("ERR", "Processing failed", it)
             }
-
     }
-
 }
